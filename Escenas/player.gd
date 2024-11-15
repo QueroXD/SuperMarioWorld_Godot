c729bdb4e835508800e26eb94c99412d331e47d2
Marc004 <marcvidal234@gmail.com>
2024-11-15 16:32:16 +0100

    Animación Caminar
    Frames de cuando camina, falta revisar lado izquierdo y cuando salta.

--- Escenas/player.gd
+++ Escenas/player.gd
@@ -6,6 +6,8 @@ const MOVE_SPEED = 200.0
 const JUMP_FORCE = -500.0
 const MAX_FALL_SPEED = 900.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	# Gravedad
 	if not is_on_floor():
@@ -18,11 +20,38 @@ func _physics_process(delta):
 	velocity.x = input_direction * MOVE_SPEED
 
 	# Salto
-	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = JUMP_FORCE
 
 	# Límite de velocidad de caída
 	velocity.y = clamp(velocity.y, -INF, MAX_FALL_SPEED)
+
+	# Animaciones
+	if input_direction != 0:
+		# Movimiento horizontal
+		if input_direction > 0: # Derecha
+			animated_sprite.animation = "walk_right"
+			animated_sprite.flip_h = false
+		elif input_direction < 0: # Izquierda
+			animated_sprite.animation = "walk_left"
+			animated_sprite.flip_h = true
+		animated_sprite.play()
+	elif not is_on_floor():  # En el aire
+		if input_direction > 0: #Derecha
+			animated_sprite.animation = "jump_right"
+			animated_sprite.play()
+		elif  input_direction < 0: #Izquierda
+			animated_sprite.animation = "jump_left"
+			animated_sprite.play()
+	else:
+		# Estado inactivo
+		#if input_direction > 0: #Derecha
+			animated_sprite.animation = "idle_right"
+			animated_sprite.play()
+		#elif input_direction < 0:
+			#animated_sprite.animation = "idle_left"
+			#animated_sprite.play()
+
 
 	# Movimiento
 	move_and_slide()
